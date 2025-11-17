c3c024fe400fbe132bbce5d65748580384a13dcc
Danziger <Pafik2005@wp.pl>
2025-11-17 22:00:59 +0100

    faza GREEN staż przypadki brzegowe

--- MPR2/src/main/java/service/TenureService.java
+++ MPR2/src/main/java/service/TenureService.java
@@ -10,10 +10,14 @@ public class TenureService {
 
     public long getTenureInYears(Employee employee, LocalDate currentDate) {
 
-
+        if (employee.getDateOfHire().isAfter(currentDate)) {
+            return 0; // Test oczekiwał 0
+        }
+        
         Period period = Period.between(employee.getDateOfHire(), currentDate);
 
 
         return period.getYears();
     }
+
 }
