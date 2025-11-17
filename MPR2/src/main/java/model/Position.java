dcd4eb37ac2f1ae7c403b4d79b89fe7b69d4ec7e
Danziger <Pafik2005@wp.pl>
2025-11-17 21:31:54 +0100

    Faza Blue REFAKTOR

--- MPR2/src/main/java/model/Position.java
+++ MPR2/src/main/java/model/Position.java
@@ -1,13 +1,13 @@
 package model;
 
-// Dodajemy 'maxSalary' i zmieniamy 'hierarchyLevel' na bardziej logiczny
+
 public enum Position {
-    // PREZES(baza, max, hierarchia) - im niższa liczba, tym wyżej
+
     PREZES(25000, 40000, 1),
     WICEPREZES(18000, 28000, 2),
     MANAGER(12000, 19000, 3),
     PROGRAMISTA(8000, 14000, 4),
-    STAZYSTA(3000, 5000, 5); // Stażysta ma najniższy poziom (najwyższą liczbę)
+    STAZYSTA(3000, 5000, 5); 
 
     private final double baseSalary;
     private final double maxSalary;
