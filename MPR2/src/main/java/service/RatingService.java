2238edc0b4197220927bf7e23ae027f15745b8f9
Danziger <Pafik2005@wp.pl>
2025-11-17 21:50:12 +0100

    faza GREEN obliczanie średniej ocen

--- MPR2/src/main/java/service/RatingService.java
+++ MPR2/src/main/java/service/RatingService.java
@@ -2,11 +2,26 @@ package service;
 
 import model.Employee;
 
+import java.util.List;
+
 
 public class RatingService {
 
 
     public void addRating(Employee employee, int rating) {
         employee.addRating(rating);
+    }
+    public double getAverageRating(Employee employee) {
+
+        List<Integer> ratings = employee.getRatingHistory();
+
+
+        double sum = 0;
+        for (int rating : ratings) {
+            sum += rating;
+        }
+
+        // Dzielimy (12 / 3 = 4.0)
+        return sum / ratings.size();
     }
 }
