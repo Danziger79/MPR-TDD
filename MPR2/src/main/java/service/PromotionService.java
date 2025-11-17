27746d2d2d67ab5fdc6ab7ff9625aba5b746366f
Danziger <Pafik2005@wp.pl>
2025-11-17 21:22:54 +0100

    faza GREEN walidacja awansu
    .

--- MPR2/src/main/java/service/PromotionService.java
+++ MPR2/src/main/java/service/PromotionService.java
@@ -3,16 +3,23 @@ package service;
 import model.Employee;
 import model.Position;
 
-
 public class PromotionService {
-
 
     public void promote(Employee employee, Position newPosition) {
 
-        // Test sprawdza czy stanowisko się zmieniło
-        employee.setPosition(newPosition);
 
-        // Test sprawdza czy pensja to nowa baza
+        int oldLevel = employee.getPosition().getHierarchyLevel();
+        int newLevel = newPosition.getHierarchyLevel();
+
+
+        if (newLevel >= oldLevel) {
+
+            throw new IllegalArgumentException("Awans musi być na wyższe stanowisko");
+        }
+
+
+
+        employee.setPosition(newPosition);
         employee.setSalary(newPosition.getBaseSalary());
     }
 }
