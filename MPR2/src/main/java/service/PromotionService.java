07124625c9d6d7555ac6e65c4d28519282c86b82
Danziger <Pafik2005@wp.pl>
2025-11-17 21:36:00 +0100

    faza GREEN walidacja podwyżek

--- MPR2/src/main/java/service/PromotionService.java
+++ MPR2/src/main/java/service/PromotionService.java
@@ -24,9 +24,23 @@ public class PromotionService {
     }
 
     public void giveRaise(Employee employee, double percentage) {
+
+
+        if (percentage < 0) {
+            throw new IllegalArgumentException("Podwyżka nie może być ujemna");
+        }
+
+
         double currentSalary = employee.getSalary();
         double raiseAmount = currentSalary * (percentage / 100.0);
         double newSalary = currentSalary + raiseAmount;
+
+
+        double maxSalary = employee.getPosition().getMaxSalary();
+        if (newSalary > maxSalary) {
+            newSalary = maxSalary;
+        }
+
 
         employee.setSalary(newSalary);
     }
